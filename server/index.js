b96a034eb332141c60b37a7991e0299ec2266492
muchammadwr <muchammadwr@gmail.com>
2024-03-29 19:48:21 +0700

    I CORS

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
-import express, { response } from "express";
+import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow all origins with Default of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   return res.status(200).send("Welcome to Mern Stack");
